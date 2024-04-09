973f74f2a1f6b761a1f49951361e055c2fee92f6
Daniel Flores <vflorestrejo@gmail.com>
2024-04-09 00:45:17 -0600

    Creacion de carpetas para separacion MVC

---
+++ CrmJovenes.Utilidades/DS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmJovenes.Utilidades
+{
+    public static class DS
+    {
+    }
+}
